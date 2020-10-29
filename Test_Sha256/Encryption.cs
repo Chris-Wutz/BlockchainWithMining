70f2fc0cb06a9c60ea19b2d26a1106db952eb7d9
Chris-Wutz <44674666+Chris-Wutz@users.noreply.github.com>
2020-10-29 09:51:27 +0100

    Update Encryption.cs

--- Test_Sha256/Encryption.cs
+++ Test_Sha256/Encryption.cs
@@ -11,21 +11,19 @@ namespace Test_Sha256
     {
         public static string ToHex(byte[] bytes, bool upperCase, bool toString)
         {
-            StringBuilder result = new StringBuilder(bytes.Length * 2);
-            for (int i = 0; i < bytes.Length; i++)
-            {
+            var result = new StringBuilder(bytes.Length * 2);
+            for (var i = 0; i < bytes.Length; i++)
                 if (toString)
                     result.Append(bytes[i].ToString(upperCase ? "X2" : "x2"));
                 else
                     result.Append(bytes[i]);
-            }
             return result.ToString();
         }
 
         public static string SHA256HexHashString(string StringIn, bool toString)
         {
             string hashString;
-            using (var sha256 = SHA256Managed.Create())
+            using (var sha256 = SHA256.Create())
             {
                 var hash = sha256.ComputeHash(Encoding.Default.GetBytes(StringIn));
                 hashString = ToHex(hash, false, toString);
