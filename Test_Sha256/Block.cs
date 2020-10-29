fbdfaf7ad6c62dc090142ac81999727be9a46b7d
Chris-Wutz <44674666+Chris-Wutz@users.noreply.github.com>
2020-10-29 09:49:55 +0100

    Update Block.cs

--- Test_Sha256/Block.cs
+++ Test_Sha256/Block.cs
@@ -8,35 +8,41 @@ namespace Test_Sha256
 {
     public class Block
     {
-        private long timestamp = DateTime.Now.Ticks;
-        public int blockNo { get; set; } = 0;
-        public int nonce { get; set; } = 0;
-        public string data { get; set; }
-        public Block next_Block { get; set; } = null;
-        public Block previous_Block { get; set; } = null;
-        public string block_hash { get; set; }
-        public Block(string data)
+       private readonly long timestamp = new DateTime(1990, 3, 4).Ticks;
+
+        public Block(string data, int blocknumber)
         {
             this.data = data;
+            blockNo = blocknumber;
         }
+
+        public int blockNo { get; set; }
+        public int nonce { get; set; } = 0;
+        public string data { get; set; }
+        public Block next_Block { get; set; }
+        public Block previous_Block { get; set; }
+        public string block_hash { get; set; }
 
         public void addNewNext(Block newBlock)
         {
-            if(this.next_Block == null)
+            if (next_Block == null)
             {
                 next_Block = newBlock;
+                next_Block.previous_Block = this;
+                Console.WriteLine(ToString());
             }
             else
             {
-                this.next_Block.addNewNext(newBlock);
+                next_Block.addNewNext(newBlock);
             }
         }
 
         public string hash(bool toString)
         {
-            string hexString = String.Empty;
+            var hexString = string.Empty;
             if (previous_Block != null)
-                hexString = Encryption.SHA256HexHashString(nonce + data + previous_Block.block_hash + timestamp + blockNo, toString);
+                hexString = Encryption.SHA256HexHashString(
+                    nonce + data + previous_Block.block_hash + timestamp + blockNo, toString);
             else
                 hexString = Encryption.SHA256HexHashString(nonce + data + timestamp + blockNo, toString);
             return hexString;
@@ -44,7 +50,8 @@ namespace Test_Sha256
 
         public override string ToString()
         {
-            return $"Block Hash: {hash(true)} \n BlockNo: {blockNo} \n Block Data: {data} \n Hashes: {nonce} \n---------------";
+            return
+                $"Block Hash: {hash(true)} \nBlockNo: {blockNo} \nBlock Data: {data} \nHashes: {nonce}\n---------------";
         }
     }
 }
